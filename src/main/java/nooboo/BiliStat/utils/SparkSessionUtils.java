5ee62ccd06bfab07b6a1ae492d69344a85c054a4
NOOBOO_real <13123724+nooboo-real@user.noreply.gitee.com>
2024-01-29 18:18:20 +0800

    draft

---
+++ src/main/java/nooboo/BiliStat/utils/SparkSessionUtils.java
@@ -0,0 +1,17 @@
+package nooboo.BiliStat.utils;
+
+import org.apache.spark.sql.SparkSession;
+
+public class SparkSessionUtils {
+
+    public static SparkSession createSparkSession(String uriConst, String uri) {
+        return SparkSession.builder()
+                .appName("BiliStat")
+                .master("local")
+                .config("spark.executor.extraJavaOptions", "-Dfile.encoding=UTF-8")
+                .config("spark.driver.extraJavaOptions", "-Dfile.encoding=UTF-8")
+                .config(uriConst, uri)
+                .getOrCreate();
+    }
+
+}
