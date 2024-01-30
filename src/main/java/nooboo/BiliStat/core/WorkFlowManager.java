7f70adb8cc268147e6bea88914501da76fca5f34
Bug-Writer <760393143@qq.com>
2024-01-30 23:43:17 +0800

    支持配置文件

--- src/main/java/nooboo/BiliStat/core/WorkFlowManager.java
+++ src/main/java/nooboo/BiliStat/core/WorkFlowManager.java
@@ -8,11 +8,11 @@ public class WorkFlowManager {
         // ... 其他初始化代码 ...
 
         // 根据需要创建不同的DataReader和DataConsumer实例
-        DataReader dataReader = ClassUtil.instantiate("nooboo.BiliStat.consumer.HBaseDataReader", DataReader.class);
-        KafkaDataConsumer dataConsumer = ClassUtil.instantiate("nooboo.BiliStat.consumer.KafkaDataConsumer", KafkaDataConsumer.class);
+        KafkaDataConsumer kafkaDataConsumer = ClassUtil.instantiate(new Configuration().getDatastoreConsumerClassName(), KafkaDataConsumer.class);
+        DataReader dataReader = ClassUtil.instantiate(new Configuration().getDatastoreReaderClassName(), DataReader.class);
 
         // 使用接口方法而不是具体类
-        dataConsumer.consumeData();
+        kafkaDataConsumer.consumeData();
         dataReader.readData();
 
         // ... 后续处理 ...
